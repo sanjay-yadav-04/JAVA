5504ad451b5a560cac6759f50360080ff9abd566
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-08-03 23:40:40 +0530

    m

---
+++ Arr.java
@@ -0,0 +1,42 @@
+// // public class Arr {
+// //     public static void main(String[]args){
+// //    //Arrays Initialisation
+// //     // int arr[];
+// //     // int arr1[]={1,2,3,4,5};
+
+// //     //Types of declaration
+// //     // int arr[];
+// //     // int []arr;
+// //     // int []arr1;
+// //     // int []arr2;
+
+// //     // int []arr=new int[];
+// //     // int []arr=new int1[5];
+// //     // int []arr=new int[]{1,2,3,4,5};
+// //     // for(int i:arr)
+// //     //     System.out.println(i);
+
+  
+// // //   int []arr=new int[]{1,2,3,4,5,6,7};
+// // //  int mid=arr.length/2;
+// // //  System.out.println(arr[mid]);
+    
+// //     }
+// // }
+
+
+// //encapsulation merger member variable into member function
+// //abstraction =nessasary info show unnesesary hide
+
+// public class Arr {
+    
+//     int a;
+//     void set(int a){
+//         this.a=a;
+//         System.out.println(this.a);
+//     }
+//     public static void main(String[] args) {
+//         Arr obj=new Arr();
+//         obj.set(10);
+//     }
+// }
