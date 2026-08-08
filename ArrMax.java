04847f2f5220433ed45d7171223477a5a6a2b766
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-08-08 23:55:36 +0530

    m

---
+++ ArrMax.java
@@ -0,0 +1,65 @@
+// class ArrMax {
+//     public static void main(String[]args)
+//     {
+//         int arr[]={110,12,512,14,15};
+//         int max=arr[3];
+//         for(int i=0;i<arr.length;++i){
+//             if(arr[i]>max){
+//                 max=arr[i];
+//             }
+//         }
+//         System.out.println(max);
+//     }
+// }
+
+
+
+
+
+
+//Missing valur of array finding
+// class ArrMax {
+//     public static void main(String[]args)
+//     {
+//         int arr[]={1,3,5,7,9,11};
+//         for(int i=0;i<arr.length-1;++i){
+//             if(arr[i+1]-arr[i]>1)
+//             {
+//                 System.out.println(arr[i]+1);
+//             }
+//         }
+        
+//     }
+// }
+
+
+
+
+
+
+//Multiple Missing valur of array finding
+// class ArrMax {
+//     public static void main(String[]args)
+//     {
+//         int arr[]={1,5,7,11,21};
+//         for(int i=0;i<arr.length;++i)
+//         {
+//             if(arr[i+1]-arr[i]>1)
+//             for(int j=arr[i]+1;j<arr[i+1];++j)
+//             System.out.println(j);
+//         }
+        
+//     }
+// }
+
+
+
+
+//Swapping Elements Of An Array
+class ArrMax {
+    public static void main(String[]args)
+    {
+      
+    }
+}
+
