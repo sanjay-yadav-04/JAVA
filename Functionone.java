f1cdf6d52507316882c32f8ca9f0953a4fc66539
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-08-20 16:22:37 +0530

    m

---
+++ Functionone.java
@@ -0,0 +1,60 @@
+// import java.util.Scanner;
+// class Functionone{
+//    static void sum(){
+//         int a=10;
+//         int b=20;
+
+//         System.out.println(a+b);
+//     }
+//     public static void main(String args[]){
+//         Scanner obj =new Scanner(System.in);
+//         sum();
+//     }
+// }
+//  int type 
+
+// import java.util.Scanner;
+// class Functionone{
+//    static int  sum(){
+//         int a=10;
+//         int b=20;
+
+//    return a+b;
+//     }
+//     public static void main(String args[]){
+//         Scanner obj =new Scanner(System.in);
+//         System.out.println(sum());
+//     }
+// }
+
+//   string type
+// import java.util.Scanner;
+// class Functionone{
+//    static String sum(){
+//         int a=10;
+//         int b=20;
+
+//    return "welcome to home";
+//     }
+//     public static void main(String args[]){
+//         Scanner obj =new Scanner(System.in);
+//         System.out.println(sum());
+//     }
+// }
+
+// boolen 
+
+
+import java.util.Scanner;
+class Functionone{
+   static boolen sum(){
+        int a=10;
+        int b=20;
+
+       return true;
+    }
+    public static void main(String args[]){
+        Scanner obj =new Scanner(System.in);
+        System.out.println(sum());
+    }
+}
