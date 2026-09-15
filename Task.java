134d9537619a860f7aa2c1916cb061ea2fd3d014
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-09-15 23:47:20 +0530

    m

---
+++ Task.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public abstract class Task {
+    static int sum(int number)
+    {
+       int sum=0;
+for(int i=1;i<= int number;i++){
+sum=sum+i;
+return sum;
+}
+    }
+    public static void main(String[]args)
+    {
+        System.out.println("Enter the Number:");
+       Scanner input = new Scanner(System.in);
+      number=input.nextInt();
+     System.out.println(sum());
+    }
+}
