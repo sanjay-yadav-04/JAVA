62fe5f7dd0cbc22286a363a33cd0ebc3f58dd494
sanjay-yadav-04 <im.sanjay.yadav.04@gmail.com>
2026-09-19 08:41:14 +0530

    m

---
+++ Third.java
@@ -0,0 +1,112 @@
+// import java.util.Scanner;
+// class Third{
+//     public static void main(String args[]){
+//     Scanner obj=new Scanner(System.in);
+
+//     int a;
+//     int b;
+
+//     System.out.println("enter first number");
+//     a=obj.nextInt();
+//     System.out.println("enter second number");
+//     b=obj.nextInt();
+
+//     System.out.println(a+b);
+//     }
+// }
+// import java.util.Scanner;
+// class Third{
+//     public static void main(String args[]){
+//     Scanner obj=new Scanner (System.in);
+
+//     int a;
+//     int b;
+
+//     System.out.println("enter first number number");
+//     a=obj.nextInt();
+//     System.out.println("enter second number");
+//     b=obj.nextInt();
+//     System.out.println(a-b);
+
+//     }
+// }
+
+
+//  correct code
+
+
+// import java.util.Scanner;
+// class Third{
+//     public static void main(String args[]){
+//         Scanner obj=new Scanner(System.in);
+//         int a;
+//         int b;
+//         System.out.println("enter a number");
+//         a=obj.nextInt();
+//         System.out.println("enter second number");
+//         b=obj.nextInt();
+//         System.out.println(a * b);
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// class Third {
+//     public static void main(String args[]) {
+//         Scanner obj = new Scanner(System.in);
+//         int a;
+//         int b;
+//         System.out.println("Enter a number");
+//         a = obj.nextInt();
+//         System.out.println("Enter second number");
+//         b = obj.nextInt();
+//         System.out.println(a * b);
+//     }
+// }
+
+
+// import java.util.Scanner;
+// class Third{
+//     public static void main(String args[]){
+//         Scanner obj=new Scanner(System.in);
+//         int a;
+//         int b;
+//         System.out.println("enter a number");
+//         a=obj.nextInt();
+//         System.out.println("enter a number");
+//         b=obj.nextInt();
+
+//         System.out.println(a/b);
+//     }
+// }
+
+// import java.util.Scanner;
+
+// class Third{
+//     public static void main(String args[]){;
+//     Scanner obj=new Scanner(System.in);
+//     int a;
+//     int b;
+
+//     System.out.println("enter a number");
+//     a=obj.nextInt();
+//     System.out.println("enter a second number");
+//     b=obj.nextInt();
+
+//     System.out.println(a%b);
+// }
+// }
+
+
+import java.util .Scanner;
+
+class Third{
+    public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
+        character='A';
+        character='B';
+
+        S
+    }
+}
